9018e8ec5ef8ffd1280bb535f42961f16a598af4
Tony Bergholtz <tony.bergholtz@gmail.com>
2018-10-16 12:45:31 +0200

    oppdatert db.js

--- db.js
+++ db.js
@@ -1,4 +1,17 @@
+const pg = require("pg");
 
+const db = {}
 
+db.select = function(query){
+    
+}
+
+db.delete = function(query){
+    
+}
+
+db.update = function(query){
+    
+}
 
 module.exports = {};
