995d759c1d2f032186ba0a218b3d5893b45bf8ef
Tony Bergholtz <tony.bergholtz@gmail.com>
2018-10-16 14:35:41 +0200

    prøver å fikse struktur

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = ('port', (process.env.PORT || 8080));
+const db = require("/js/db.js");
+
+
 
 app.set('port', port);
 app.use(express.static('Public'));
@@ -11,9 +14,34 @@ app.listen(app.get('port'), function() {
     
 });
 
-    
+
 
 const users = [];
+
+let userEmail = req.body.email;
+let userName = req.body.name;
+let passwordHash = req.body.pswHash;
+let userRole = req.body.role;
+
+app.post('/app/users',function(req,res,next){
+    let query`INSERT INTO "public"."Users"("email", "username", "hash", "role")
+    VALUES('${userEmail}', '${userName}', '${passwordHash}', ${userRole}) RETURNING "id", "email", "hash", "role"`
+    
+    let code = db.insert(query) ? 200:500;
+    res.status(code).json({}).end();
+})
+
+app.get('/app/user',function(req,res,next){
+    
+    let query = "Select * from Users";
+    let users = db.select(query;)
+        
+    if(users){
+       res.status(200).json(JSON.parse(users));
+    }else{
+        //???
+                            }
+});
 
 app.post("/api/user",function(req,res){
     
