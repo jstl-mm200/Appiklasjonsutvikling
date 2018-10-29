b1e2ae11cd3da24db119024e9f4cd94bf1e47d39
Tony Bergholtz <tony.bergholtz@gmail.com>
2018-10-29 11:37:59 +0100

    update

---
+++ js/requestQue.js
@@ -0,0 +1,52 @@
+var express = require('express')
+var router = express.Router();
+var db = require("./db.js");
+
+let userNames = [];
+
+function isNameInList(list,name){
+    let searchName = name.toLowerCase();
+    let result = false;
+    for(let user in list){
+		if(list[user].toLowerCase() === searchName){
+            result = true;
+            break;
+		}
+    }
+	return result;
+}
+
+
+router.get('/app/requests', function(req,res, next){
+    // Get all requests in the DB that have not ben responded to.
+    let sqlQuery = "Select * from requests where answerd is NULL"
+    let requests = db.select(sqlQuery);
+    res.json(requests).end();
+});
+
+router.post('/app/request', function (req,res,next){
+
+    ///TODO: Is the request unique?
+
+    // Get the request details from the http request. (sent from the client)
+    let userName = req.body.userName;
+    let userID = req.body.userID;
+    let description = req.body.description;
+
+    // Build sql query for inserting request in DB.
+    let sqlQuery = `INSERT INTO "public"."Users"("email", "username", "hash") 
+        VALUES('${userEmail}', '${userName}', '${paswordHash}') RETURNING *`;
+    
+    let request = db.insert(sqlQuery);
+
+    if(request){
+        res.status(200).json(request).end();
+    } else{
+        res.status(500).json({}).end();
+        
+    }
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const db = require("./js/db.js");
 const user = require("./js/users.js");
+
+let userNames = [];
 
 
 app.set('port', (process.env.PORT || 8080));
@@ -9,10 +12,27 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(user);
 
+app.get('/app/requests', function(req,res, next){
+    res.json(userNames).end();
+});
+
+app.post('/app/request', function (req,res,next){
+    let userName = req.body.userName;
+    let isUnique = ! isNameInList(userNames, userName);
+
+    if(isUnique && userName){
+        userNames.push(req.body.userName);
+        res.status(200).json(userNames).end();
+    } else{
+        res.status(400).json(userNames).end();
+        res.se
+    }
+});
+
 function isNameInList(list,name){
     let searchName = name.toLowerCase();
     let result = false;
-    for(let student in list){
+    for(let user in list){
 		if(list[user].toLowerCase() === searchName){
             result = true;
             break;
