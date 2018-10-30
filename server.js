ad38f340ab17cc8518ff6b96b7b1d5753b7b3c13
Tony Bergholtz <tony.bergholtz@gmail.com>
2018-10-30 15:59:42 +0100

    index er fikset

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-//const db = require("./js/db.js"); 
+const db = require("./js/db.js"); 
 const user = require("./js/users.js"); 
-//const que = require("./js/requestQue.js") 
+const que = require("./js/requestQue.js") 
 
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(user);
-//app.use(que);
+app.use(que);
 
 
 
