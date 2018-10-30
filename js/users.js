a9e661f900d52b54fd6683d1466fcab3fc41154e
Tony Bergholtz <tony.bergholtz@gmail.com>
2018-10-30 12:39:42 +0100

    fix

--- js/users.js
+++ js/users.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var router = express.Router();
-var db = require("./db.js");
+//var db = require("./db.js");
 
 router.get('/app/users',function(req,res,next){
     let query = "Select * from Users";
