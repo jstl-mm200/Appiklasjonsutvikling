f90bb3661edb3bee916059cb630b93a7c2cba706
Tony Bergholtz <tony.bergholtz@gmail.com>
2018-11-06 12:44:37 +0100

    fix

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const db = require("./js/db.js"); 
 const authorize = require("./authorize.js");
-const user = require("./js/users.js"); 
+const users = require("./js/users.js"); 
 const que = require("./js/requestQue.js") 
 
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static('Public'));
 app.use(bodyParser.json());
-app.use(user);
+app.use(users);
 app.use(que);
 
 
