044b35e4e43588568b9f6b118a1a78987e214d37
Tony Bergholtz <tony.bergholtz@gmail.com>
2018-11-24 16:38:32 +0100

    jj

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('port', (process.env.PORT || 3000));
 app.use(express.static('Public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+  extended: true
 }));
 
 app.use(function(err, req, res, next) {
